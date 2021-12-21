fc73ca3e381f85147b02943c183333ad49f79280
Hugo Bois <h.bois@scorf.io>
2021-12-21 15:28:05 +0100

    fixed: maxLevel to integer and superTroopIsActive

--- src/main/java/com/lycoon/clashapi/models/player/Troop.kt
+++ src/main/java/com/lycoon/clashapi/models/player/Troop.kt
@@ -1,12 +1,13 @@
 package com.lycoon.clashapi.models.player
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class Troop(
-    val level: Int,
     val name: String,
-    val maxLevel: String,
+    val level: Int,
+    val maxLevel: Int,
     val village: String,
-    val superTroopIsActive: Boolean? = false
+    @SerialName("superTroopIsActive") val isSuperTroopActive: Boolean? = false
 )
